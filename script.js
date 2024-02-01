3ca119de2873b0e56d35a5b45ae265ce2aa9bb03
MiniDonutRR <147582979+MiniDonutRR@users.noreply.github.com>
2024-02-02 07:39:49 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,6 @@
-document.addEventListener('DOMContentLoaded', function () {
-    const body = document.body;
-    const navbar = document.getElementById('navbar');
-    const navLinks = document.querySelectorAll('.nav-links a');
-
-    
-        }
-        
-    });
-});
-
+function toggleMenu() {
+  const menu = document.querySelector(".menu-links");
+  const icon = document.querySelector(".hamburger-icon");
+  menu.classList.toggle("open");
+  icon.classList.toggle("open");
+}
