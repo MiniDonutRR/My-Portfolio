60d4bdbb366af8e616a1aee79761fe4f657bc811
MiniDonutRR <147582979+MiniDonutRR@users.noreply.github.com>
2024-02-02 07:34:49 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,20 +1,9 @@
 document.addEventListener('DOMContentLoaded', function () {
-    const nightModeToggle = document.getElementById('nightModeToggle');
     const body = document.body;
     const navbar = document.getElementById('navbar');
     const navLinks = document.querySelectorAll('.nav-links a');
 
     
-    
-    nightModeToggle.addEventListener('change', function () {
-        if (this.checked) {
-            body.classList.add('night-mode');
-            navbar.classList.add('night-mode');
-            navLinks.forEach(link => link.classList.add('night-mode'));
-        } else {
-            body.classList.remove('night-mode');
-            navbar.classList.remove('night-mode');
-            navLinks.forEach(link => link.classList.remove('night-mode'));
         }
         
     });
